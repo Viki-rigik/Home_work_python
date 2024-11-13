622bfa69c46350c18c0df9689f0995cfbfbab2dc
Viki-rigik <nviki748@gmail.com>
2024-11-13 18:57:17 +0200

    Add files via upload

---
+++ Генераторна функція_10.1.py
@@ -0,0 +1,16 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    for _ in range(end):
+        yield begin
+        begin = func(begin)
+def pow(x):
+    return x ** 2
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
