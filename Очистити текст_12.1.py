47f8f291ff44c2edc3a8a6e45e7f802dadac554d
Viki-rigik <nviki748@gmail.com>
2024-11-19 15:03:22 +0200

    Add files via upload

--- Очистити текст_12.1.py
+++ Очистити текст_12.1.py
@@ -4,12 +4,12 @@ import re
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'r', 'utf-8') as file:
-        html = file.read()
-    cleaned = re.sub(r'<.*?>', '', html)
+        html_content = file.read()
+    file_content = re.sub(r'<[^>]*>', '', html_content)
     with codecs.open(result_file, 'w', 'utf-8') as file:
-        file.write(cleaned)
+        file.write(file_content)
 
     print(f"The cleaned file is saved in '{result_file}'")
 
-delete_html_tags('draft.html')
+
 
