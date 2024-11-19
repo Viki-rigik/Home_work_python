e83f3ee40b4b4a2e22744e29e7c03122861a4961
Viki-rigik <nviki748@gmail.com>
2024-11-19 13:40:12 +0200

    Add files via upload

---
+++ Перевірка на парність_11.3.py
@@ -0,0 +1,7 @@
+def is_even(number):
+   last_value = str(number)[-1]
+   return last_value in '02468'
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
